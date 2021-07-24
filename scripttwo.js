881de502e9dda7ac9217cc5f98767174a38ce0da
Mahmoud Abu-Attiya <76923704+mahmoud-abu-attiya@users.noreply.github.com>
2021-07-24 06:42:13 +0200

    Update scripttwo.js

--- scripttwo.js
+++ scripttwo.js
@@ -1,3 +1,15 @@
+let logo = document.querySelector(".logo");
+
+logo.addEventListener("click" , (e)=>{
+    document.getElementById("secondary").classList.remove("show");
+    document.getElementById("home").classList.add("show");
+    document.querySelector("body").classList.add("body-bg");
+    document.querySelectorAll(".ancors a").forEach((a)=>{
+        a.classList.remove("active");
+    })
+})
+
+
 let td = document.querySelectorAll(".td div");
 td.forEach( (element)=> {
     element.addEventListener("click",(e)=> {
@@ -8,22 +20,77 @@ td.forEach( (element)=> {
     })
 });
 
+let pri = document.querySelector(".ancors-home .pri");
+let dash = document.querySelector(".ancors-home .dash");
+let photo = document.getElementById("animated")
+
+pri.addEventListener("click" , (e)=>{
+    document.getElementById("secondary").classList.add("show");
+    document.getElementById("home").classList.remove("show");
+    document.querySelector("body").classList.remove("body-bg");
+    // document.querySelector(".ancors .pri").click()
+    // open pricing
+    document.querySelector(".half:nth-child(2)").classList.add("op");
+    document.querySelector(".half:nth-child(1)").classList.remove("op");
+    document.querySelector(".bba").classList.remove("d");
+    document.querySelector(".ancors .pri").classList.add("active");
+});
+
+dash.addEventListener("click" , (e)=>{
+    document.getElementById("secondary").classList.add("show");
+    document.getElementById("home").classList.remove("show");
+    document.querySelector("body").classList.remove("body-bg");
+    // document.querySelector(".ancors .dash").click();
+    document.querySelector(".half:nth-child(2)").classList.remove("op");
+    document.querySelector(".half:nth-child(1)").classList.add("op");
+    document.querySelector(".bba").classList.add("d");
+    document.querySelector(".ancors .dash").classList.add("active");
+
+});
+
 let Aactives = document.querySelectorAll(".ancors a");
 Aactives.forEach((active)=>{
-        active.addEventListener("click", (e)=>{
-            Aactives.forEach((re)=>{re.classList.remove("active")})
+    active.addEventListener("click", (e)=>{
+        Aactives.forEach((re)=>{re.classList.remove("active")})
             e.target.classList.add("active");
             if(e.target.classList.contains("pri")){
-                document.querySelector("main").classList.remove("trf");
                 document.querySelector(".half:nth-child(2)").classList.add("op");
                 document.querySelector(".half:nth-child(1)").classList.remove("op");
                 document.querySelector(".bba").classList.remove("d");
+                if(!document.querySelector(".btns div i").classList.contains("down")){
+                    document.querySelector(".btns div").click()
+                }
+                
+                document.querySelector("#one .producer-img").style.opacity = "0"
+                photo.classList.remove('car-an')
+                setTimeout(()=>{
+                    photo.classList = "car-an-pri"
+                }, 0)
+                
+                setTimeout(()=>{
+                    document.querySelector("#one .producer-img").style.opacity = "1"
+                    photo.classList.add("car-an");
+                }, 300);
+                
+                
             }
             else{
-                document.querySelector("main").classList.add("trf");
                 document.querySelector(".half:nth-child(2)").classList.remove("op");
                 document.querySelector(".half:nth-child(1)").classList.add("op");
                 document.querySelector(".bba").classList.add("d");
+                if(document.querySelector(".btns div i").classList.contains("down")){
+                    document.querySelector(".wightcar img").classList = "secondary"
+                    photo.classList.remove('car-an')
+                setTimeout(()=>{
+                    photo.classList = "car-an-dash"
+                }, 0)
+                setTimeout(()=>{
+                    document.querySelector(".wightcar img").classList = ""
+                    photo.classList.add("car-an");
+                }, 300);
+                }else{
+                    photo.classList = "car-an-dash car-an"
+                }
             }
     });
 });
@@ -64,11 +131,4 @@ btnsPackage[2].addEventListener("click",(e)=>{
     three.classList.remove("hidden");
     
 })
-
-if (window.location.href.indexOf('#') != -1 && window.location.hash == "#Dashboard"){
-    document.querySelector(".ancors .dash").click()
-}else{
-    window.location.hash = "#Pricing"
-    document.querySelector(".ancors .pri").click()
-}
 
